ffd463b4ba0c580a90318630e81e9c1fcabc09ab
Balloon <Podginator@gmail.com>
2017-05-02 21:03:16 +0100

    TypeInformation added

---
+++ TypeInformation/LambdaTypeInformation.cpp
@@ -0,0 +1,23 @@
+#include "LambdaTypeInformation.h"
+
+using namespace std;
+using namespace RattleLang;
+
+
+LambdaTypeInformation::LambdaTypeInformation() : TypeInformation(){}
+
+// The Constructor, takes a variable amount of types.
+LambdaTypeInformation::LambdaTypeInformation(std::initializer_list<RattleLang::type> types, Context* context = nullptr)
+: TypeInformation(types, context){}
+
+
+string LambdaTypeInformation::get_c_return_types() {
+    std::string res = "std::function<" + this->get_c_typename() + "(";
+    if (inner_vars.size() > 0){
+        res.append(StringHelper::combine_str_ptr(inner_vars, ',',
+                                                 &NamedVariableInfo::second, &TypeInformation::get_c_typename));
+    }
+    res += ")>";
+    return res;
+}
+
--- TypeInformation/LambdaTypeInformation.h
+++ TypeInformation/LambdaTypeInformation.h
@@ -8,27 +8,12 @@ namespace RattleLang {
     class LambdaTypeInformation : public TypeInformation {
     public:
 
-        LambdaTypeInformation() : TypeInformation(){}
+        LambdaTypeInformation();
 
         // The Constructor, takes a variable amount of types.
-        LambdaTypeInformation(std::initializer_list<RattleLang::type> types, Context* context = nullptr)
-                : TypeInformation(types, context){}
+        LambdaTypeInformation(std::initializer_list<RattleLang::type> types, Context* context = nullptr);
 
-
-        std::string get_c_return_types() override {
-            std::string res = "std::function<" + this->get_c_typename() + "(";
-            size_t param_size = inner_vars.size();
-
-            if (inner_vars.size() > 0){
-                //Bordering on absurd, really.
-                res.append(StringHelper::combine_str_ptr(inner_vars, ',',
-                                                         &NamedVariableInfo::second, &TypeInformation::get_c_typename));
-            }
-
-            res += ")>";
-
-            return res;
-        }
+        std::string get_c_return_types() override;
 
     };
 }
